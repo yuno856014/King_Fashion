39cbdbd1e6a4cecaedb2b00f9dbaa1a9318b3895
yuno856014@gmail.com <yuno856014@gmail.com>
2021-09-09 10:25:54 +0700

    up

---
+++ KingFashion/KingFashion/Commons/Common.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KingFashion.Commons
+{
+    public static class Common
+    {
+        public static string ApiUrl = "" 
+    }
+}
