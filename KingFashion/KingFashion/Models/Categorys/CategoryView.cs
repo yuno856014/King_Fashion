ad38e0daeb721a63300df3c5112e34e96e76dccb
yuno856014@gmail.com <yuno856014@gmail.com>
2021-09-09 11:26:42 +0700

    up dashboard

---
+++ KingFashion/KingFashion/Models/Categorys/CategoryView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KingFashion.Models.Categorys
+{
+    public class CategoryView
+    {
+        public int CatDetailsId { get; set; }
+        public string CatDetailsName { get; set; }
+        public bool Status { get; set; }
+        public bool IsDeleted { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
